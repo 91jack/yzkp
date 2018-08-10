b947ee19bd9f63a474f4fb15dfcd95e7cf2ae4d5
1046536189@qq.com <1046536189@qq.com>
2018-08-10 16:40:37 +0800

    111
    111

--- yzkp2/page/job/joblist/joblist.js
+++ yzkp2/page/job/joblist/joblist.js
@@ -7,8 +7,6 @@ Page({
    * 页面的初始数据
    */
   data: {
-    currentTab: [true, true, true, true],
-    navBarData: ['推荐', '地区', '职位', '要求'],
     index: 0//选择的下拉列表下标
   },
 
@@ -21,36 +19,6 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  navbarTap: function (e) {
-    var i = e.currentTarget.dataset.idx;
-    var index = this.data.currentTab[i];
-    var hid = "currentTab[" + i + "]";
-    console.log(index);
-    for (var j = 0; j < this.data.currentTab.length; j++) {
-      var act = "currentTab[" + j + "]"
-      this.setData({
-        [act]: true
-      })
-    }
-    if (index) {
-      this.setData({
-        [hid]: false
-      })
-    } else {
-      this.setData({
-        [hid]: true
-      })
-    }
-  },
-  hideBar: function (e) {
-    var i = Number(e.currentTarget.dataset.idx);
-    console.log(typeof i)
-    var index = this.data.currentTab[i];
-    var hid = "currentTab[" + i + "]";
-    this.setData({
-      [hid]: true
-    })
-  },
   // 前往公司
   toCompanyDetail: function () {
     wx.navigateTo({
