6e91257556a7ac16a92b0070ec38cd89364d96f3
1046536189@qq.com <1046536189@qq.com>
2018-08-20 09:39:04 +0800

    11
    11

--- yzkp2/page/common/resume/jobedit/jobedit.js
+++ yzkp2/page/common/resume/jobedit/jobedit.js
@@ -5,7 +5,10 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    inData:'',
+    inHide:false,
+    outData:'',
+    outHide:false
   },
 
   /**
@@ -28,39 +31,19 @@ Page({
   onShow: function () {
   
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
+  bindDateChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    var i = Number(e.currentTarget.dataset.idx)
+    if(i==0){ //入职时间
+      this.setData({
+        inDate: e.detail.value,
+        inHide:true
+      })
+    } else if (i == 1) { //离职时间
+      this.setData({
+        outDate: e.detail.value,
+        outHide: true
+      })
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
 })
