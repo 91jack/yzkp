70d15a66dc37cefd5b02110dc6e921fe1697b4cb
91jack <2764132626@qq.com>
2018-08-20 14:43:07 +0800

    1
    1

--- yzkp2/page/employee/contract/index/index.js
+++ yzkp2/page/employee/contract/index/index.js
@@ -1,32 +1,12 @@
 // page/employee/contract/index/index.js
 const allInfoUrl = require('../../../../config').allInfoUrl;
-
-const phonecodeUrl = require('../../../../config').phonecodeUrl
-
-
-// 获取验证码
-const phoneCodeUrl = require('../../../../config').phoneCodeUrl;
-// 验证码校验
-const phoneCodeyzUrl = require('../../../../config').phoneCodeyzUrl;
-
-
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    infoData: {},
-
-    timer: '',
-
-    timer: '',//定时器名字
-    yzcode: '',
-    incode: '',
-
-    timerData: '获取验证码',
-    countDownNum: 60,
-    openInterVal: true
+    infoData: null
   },
 
   /**
@@ -71,84 +51,5 @@ Page({
   onShow: function () {
 
   },
-  // 获取验证码
-  getCode: function (event) {
-    var _this = this;
-    wx.request({
-      url: phoneCodeUrl,//获取验证码
-      data: {
-        phone: 18502323596,
-        token: getApp().globalData.token,
-      },
-      success: function (res) {
-        console.log(res)
-        if (res.data.status == 0) {
-          _this.setData({
-            yzcode: res.data.obj
-          })
-
-        }
-      }
-    })
-    if (_this.data.openInterVal) {
-      this.setData({
-        timerData: '5s',
-      });
-      var time = 5;
-      var timer = setInterval(function () {
-        time--;
-        if (time > 0) {
-          _this.setData({
-            openInterVal: false,
-            countDownNum: time,
-            timerData: time + 's'
-          })
-        }
-        if (time == 0) {
-          clearInterval(timer);
-          time = 60;
-          _this.setData({
-            openInterVal: true,
-            timerData: '重新获取'
-          })
-        }
-      }, 1000)
-      timer;
-    }
-  },
-  // 获取输入的验证码
-  getIncode: function (e) {
-    this.setData({
-      incode: e.detail.value
-    })
-    console.log(e.detail.value)
-  },
-  submitBtn: function () {
-    var _this = this;
-    wx.request({
-      url: phoneCodeyzUrl,//验证验证码
-      data: {
-        token: getApp().globalData.token,
-        phone: '18502323596',
-        yzcode: _this.data.yzcode
-      },
-      success: function (res) {
-        console.log(res)
-        if (res.data.status == 0) {
-          wx.showToast({
-            title: '信息验证成功',
-            icon: 'success',
-            duration: 2000,
-            success: function () {
-              setTimeout(function () {
-                wx.navigateTo({
-                  url: '/page/common/sign/sign?employeeId=' + _this.data.infoData.id + '&id=',
-                })
-              }, 2000)
-            }
-          })
-        }
-      }
-    })
-  }
+  
 })
--- yzkp2/page/tabBar/index/index.js
+++ yzkp2/page/tabBar/index/index.js
@@ -53,7 +53,7 @@ Page({
         text: '兼职职位'
       },
       {
-        url: '/page/employee/contract/step1/step1',
+        url: '/page/employee/contract/index/index',
         src: '/image/index/discuss.png',
         text: '合同管理'
       },
