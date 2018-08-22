91a3e2ec3f146f9c6757f99853e95376d6e0d54c
91jack <2764132626@qq.com>
2018-08-22 17:28:59 +0800

    1
    1

--- yzkp2/page/common/industry/industry.js
+++ yzkp2/page/common/industry/industry.js
@@ -8,34 +8,32 @@ Page({
   data: {
     industry: null,
     currentSelect: null,
-    profession:'',
+    profession: '',
     _industrynav: '',
     _profession: '',
     _job: '',
-    id:'',
-    returnIndustryData:''
+    type: '',
+    returnIndustryData: ''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    
-    if (options.id){
+  onLoad: function(options) {
+    if (options.type) {
       this.setData({
-        id: options.id
+        type: options.type
       })
     }
-   
+
 
     var _this = this;
     wx.getStorage({
       key: 'industryArr',
-      success: function (res) {
-        console.log(res)
+      success: function(res) {
         _this.setData({
           industry: res.data,
-          currentSelect:res.data[0].list
+          currentSelect: res.data[0].list
         })
       },
     })
@@ -44,18 +42,18 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-  
+  onReady: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-  
+  onShow: function() {
+
   },
-  // 城市一级筛选
-  clickLevel1: function (e) {
+  // 行业一级筛选
+  clickLevel1: function(e) {
     var _this = this;
     console.log(e.currentTarget.dataset.industrynav)
     // console.log(e.currentTarget.dataset.value)
@@ -66,28 +64,41 @@ Page({
       currentSelect: _this.data.industry[industrynav].list
     })
   },
-  // 城市二级筛选
-  clickLevel2: function (e) {
+  // 行业二级筛选
+  clickLevel2: function(e) {
     var _this = this;
     var professionIndex = e.currentTarget.dataset.profession
     console.log(e.currentTarget.dataset.value)
-   _this.setData({
-     _profession: professionIndex,
-     profession: _this.data.currentSelect[professionIndex].list,
-     returnIndustryData: e.currentTarget.dataset.value
-   })
-    if (_this.data.id =='"intension"'){
-      wx.navigateTo({
-        url: '/page/resume/jobintensionedit/jobintensionedit?industry=' + _this.data.returnIndustryData+'',
-      })
+    _this.setData({
+      _profession: professionIndex,
+      profession: _this.data.currentSelect[professionIndex].list,
+      returnIndustryData: e.currentTarget.dataset.value
+    })
+
+
+
+    var url = '';
+    var type = this.data.type;
+    console.log(type)
+    switch (type) {
+      case 'intension': //求职意向
+        url = '/page/resume/jobintensionedit/jobintensionedit?industry=' + _this.data.returnIndustryData;
+        break;
+      case 'industry': // 职位列表
+        url = '/page/company/joblist/joblist?industry=' + _this.data.returnIndustryData;
+        break;
     }
+    console.log(url)
+    wx.navigateTo({
+      url: url
+    })
   },
-  // 区县城市选择
-  clickLevel3: function (e) {
+  // 行业3级选择
+  clickLevel3: function(e) {
     var _this = this;
     console.log(e)
     var jobIndex = e.currentTarget.dataset.job
-   
+
     this.setData({
       _job: jobIndex,
       job: _this.data.profession[jobIndex].name
--- yzkp2/page/common/msgdetail/msgdetail.js
+++ yzkp2/page/common/msgdetail/msgdetail.js
@@ -7,50 +7,17 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    input:''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    //console.log(options)
+    console.log(options)
   
-    socket.sendMessage();
+   
 
-    // // 建立连接
-    // wx.connectSocket({
-    //   url: webSocketUrl + '?type=0&id=20&head_img=1',
-    //   data: {
-    //     // type: 0,
-    //     // id: 20
-    //   },
-    //   header: {
-    //     'content-type': 'application/x-www-form-urlencoded'
-    //   },
-    //   method: 'GET',
-    // })
-
-    // //接收数据
-    // wx.onSocketMessage(function (res) {
-    //   console.log(res)
-    // })
-
-    // //连接失败
-    // wx.onSocketError(function () {
-    //   console.log('websocket连接失败！');
-    // })
-
-    // wx.onSocketOpen(function () {
-    //   console.log('WebSocket连接已打开！')
-    //   console.log(options)
-    //   var data = {
-    //     msgType: 0, resumeId: 20, companyId: 1, content: '小程序就是渣渣'
-    //   }
-
-    //   wx.sendSocketMessage({data:JSON.stringify(data)})
-    //   console.log(JSON.stringify(data))
-    // })
   },
 
   /**
@@ -100,5 +67,20 @@ Page({
    */
   onShareAppMessage: function () {
   
+  },
+  getValue:function(e){
+    console.log(e.detail.value)
+    this.setData({
+      input:e.detail.value
+    })
+  },
+  sendBtn:function(){
+    var data = {
+      msgType: 2, 
+      resumeId: 20, 
+      companyId: 1, 
+      content: this.data.input
+    }
+    socket.sendMessage(data);
   }
 })
